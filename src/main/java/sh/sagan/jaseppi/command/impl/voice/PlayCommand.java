9b331e81674d6856102d4db5a286165cfe8ba001
sagan <camjanssen120@gmail.com>
2021-09-28 20:36:15 -0600

    fix search params

--- src/main/java/sh/sagan/jaseppi/command/impl/voice/PlayCommand.java
+++ src/main/java/sh/sagan/jaseppi/command/impl/voice/PlayCommand.java
@@ -57,7 +57,7 @@ public class PlayCommand extends Command {
     private void load(Message message, String linkOrSearch) {
         String query = linkOrSearch;
         if (!linkOrSearch.startsWith("http")) {
-            query = "ytsearch:" + linkOrSearch;
+            query = "ytmsearch:" + linkOrSearch;
         }
         String finalQuery = query;
         message.getServer().ifPresent(server -> jaseppi.getAudioPlayerManager().loadItemOrdered(server, finalQuery, new AudioLoadResultHandler() {
