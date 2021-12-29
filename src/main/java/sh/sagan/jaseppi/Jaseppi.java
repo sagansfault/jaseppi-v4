c3dd57adb5b2c18f7d2365fd77db4dc0a062d734
sagan <camjanssen120@gmail.com>
2021-12-28 20:28:41 -0700

    remove dumb commands

--- src/main/java/sh/sagan/jaseppi/Jaseppi.java
+++ src/main/java/sh/sagan/jaseppi/Jaseppi.java
@@ -1,19 +1,8 @@
 package sh.sagan.jaseppi;
 
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.source.soundcloud.SoundCloudAudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 import org.javacord.api.DiscordApi;
-import org.javacord.api.audio.AudioSource;
 import sh.sagan.jaseppi.command.impl.RedditCommand;
 import sh.sagan.jaseppi.command.impl.SayCommand;
-import sh.sagan.jaseppi.command.impl.voice.DisconnectListener;
-import sh.sagan.jaseppi.command.impl.voice.JaseppiAudioPlayer;
-import sh.sagan.jaseppi.command.impl.voice.LeaveCommand;
-import sh.sagan.jaseppi.command.impl.voice.PlayCommand;
-import sh.sagan.jaseppi.command.impl.voice.RepeatCommand;
-import sh.sagan.jaseppi.command.impl.voice.SkipCommand;
 import sh.sagan.jaseppi.command.lib.CommandManager;
 import sh.sagan.jaseppi.command.lib.IntakeDelegator;
 import sh.sagan.jaseppi.function.CursedImageReply;
@@ -25,10 +14,6 @@ public class Jaseppi {
     public static final String PREFIX = ".";
 
     private final DiscordApi api;
-    private final DefaultAudioPlayerManager audioPlayerManager;
-    private final JaseppiAudioPlayer audioPlayer;
-    private final AudioSource audioSource;
-    private final YoutubeAudioSourceManager youtubeAudioSourceManager;
 
     private final CommandManager commandManager;
     private final HttpClient httpClient;
@@ -38,12 +23,6 @@ public class Jaseppi {
 
         commandManager = new CommandManager();
         httpClient = HttpClient.newBuilder().build();
-        audioPlayerManager = new DefaultAudioPlayerManager();
-        youtubeAudioSourceManager = new YoutubeAudioSourceManager(true);
-        audioPlayerManager.registerSourceManager(youtubeAudioSourceManager);
-        audioPlayer = new JaseppiAudioPlayer(audioPlayerManager);
-        audioSource = new LavaplayerAudioSource(api, audioPlayer);
-        audioPlayer.addListener(audioPlayer);
 
         initListeners();
         initCommands();
@@ -52,15 +31,9 @@ public class Jaseppi {
     private void initListeners() {
         api.addMessageCreateListener(new IntakeDelegator(commandManager));
         api.addMessageCreateListener(new CursedImageReply(this));
-        api.addServerVoiceChannelMemberLeaveListener(new DisconnectListener(this));
     }
 
     private void initCommands() {
-        commandManager.register(new PlayCommand(this));
-        commandManager.register(new LeaveCommand(this));
-        commandManager.register(new RepeatCommand(this));
-        commandManager.register(new SkipCommand(this));
-
         commandManager.register(new SayCommand());
         commandManager.register(new RedditCommand(this.httpClient));
     }
@@ -75,21 +48,5 @@ public class Jaseppi {
 
     public HttpClient getHttpClient() {
         return httpClient;
-    }
-
-    public AudioPlayerManager getAudioPlayerManager() {
-        return audioPlayerManager;
-    }
-
-    public JaseppiAudioPlayer getAudioPlayer() {
-        return audioPlayer;
-    }
-
-    public AudioSource getAudioSource() {
-        return audioSource;
-    }
-
-    public YoutubeAudioSourceManager getYoutubeAudioSourceManager() {
-        return youtubeAudioSourceManager;
     }
 }
